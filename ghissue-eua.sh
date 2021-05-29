9b3b037312b94c02cf4a7bf5a240506d4b9fed1b
samq-ws <sam.quakenbush@whitesourcesoftware.com>
2021-05-28 23:13:31 -0600

    added debug

--- ghissue-eua.sh
+++ ghissue-eua.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 APIURL=https://saas.whitesourcesoftware.com
-WS_PROJECTTOKEN=$(awk -F\" '/projectToken/ {print $4}' ./whitesource/scanProjectDetails.json)
+WS_PROJECTTOKEN=$(jq -r '.projects | .[] | .projectToken' ./whitesource/scanProjectDetails.json )
 
 ### Get CVE by Red Shield
 REDSHIELD=$(curl --request POST $APIURL'/api/v1.3' --header 'Content-Type: application/json' --header 'Accept-Charset: UTF-8'  --data-raw '{   'requestType' : 'getProjectSecurityAlertsByVulnerabilityReport',   'userKey' : '$WS_USERKEY',   'projectToken': '$WS_PROJECTTOKEN', 'format' : 'json'}' | jq '.alerts | .[] | select(.euaShield=="RED") | .vulnerabilityId')
-echo $REDSHIELD
+echo "REDSHIELD:"$REDSHIELD
 
 ## Get Github issue number by CVE
 GHISSUE=$(gh issue list -S $REDSHIELD --json number --jq '.[] | .number ')
-echo $GHISSUE
+echo "GHISSUE:"$GHISSUE
 
 ### Get keyUuid - requires productName and projectName
 KEYUUID=$(curl --request POST $APIURL'/api/v1.3' --header 'Content-Type: application/json' --header 'Accept-Charset: UTF-8'  --data-raw '{   'requestType' : 'getOrganizationEffectiveUsageAnalysis',   'userKey' : '$WS_USERKEY',   'orgToken': '$WS_APIKEY','format' : 'json'}' | jq -r '.products | .[] | select(.productName=="$WS_PRODUCTNAME") | .projects | .[] | select(.projectName=="$WS_PROJECTNAME") | .libraries | .[] | select(.resultingShield=="RED") | .keyUuid')
-echo $KEYUUID
+echo "KEYUUID:"$KEYUUID
 
 ### Get ProjectID
 PROJECTID=$(curl --request POST $APIURL'/api/v1.3' --header 'Content-Type: application/json' --header 'Accept-Charset: UTF-8'  --data-raw '{   'requestType' : 'getOrganizationEffectiveUsageAnalysis',   'userKey' : '$WS_USERKEY',   'orgToken': '$WS_APIKEY','format' : 'json'}' | jq '.products | .[] | select(.productName=="$WS_PRODUCTNAME") | .projects | .[] | select(.projectName=="$WS_PROJECTNAME") | .projectId ')
-echo $PROJECTID
+echo "PROJECTID:"$PROJECTID
 
 ### Construct Link
 EUALINK="$APIURL/Wss/WSS.html#!libraryVulnerabilities;uuid=$KEYUUID;project=$PROJECTID"
